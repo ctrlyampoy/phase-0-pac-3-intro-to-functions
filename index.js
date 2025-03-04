77640927b22dcd15905b30a1a20b994fd604e36b
Yampoy <yampoy.elvis@gmail.com>
2025-03-04 06:26:20 +0300

    JS test 3 done

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+  }
+sayHello();
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+   }
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+    }
+sayHelloToSamip();
+sayHelloToGuadalupe();
+sayHelloToLiz();
+
+function doSomething(thing) {
+    console.log(thing);
+  }
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log("Hello", "Odhiambo")
+  
+  sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+  
+  function say(greeting, firstName) {
+    console.log("greeting:", greeting);
+    console.log("firstName:", firstName);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+ say("Hello", "Julio");
+
+ function add(x, y) {
+    return x + y;
+  }
+
+  console.log(add(9000, 80));
+
+  
